26c4eb345b0e19c43b639c94464dc1af6ee0fbf7
evdoo <o.evdokima@gmail.com>
2014-07-26 10:58:03 +0400

    Initial commit

---
+++ src/androidlearning/AtomGame/GameActivity.java
@@ -0,0 +1,26 @@
+package androidlearning.AtomGame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.TextView;
+
+/**
+ * Created by Olga on 25.07.2014.
+ */
+public class GameActivity extends Activity {
+
+    public void onCreateGame(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+
+    public void setGameField() {
+        GridView gameField = (GridView) findViewById(R.id.gameField);
+
+    }
+
+    public void rayReflection() {
+
+    }
+}

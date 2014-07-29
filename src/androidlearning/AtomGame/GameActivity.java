d1c2c55ee3d30d2ae65567d4fbd8e39c63ce35ca
evdoo <o.evdokima@gmail.com>
2014-07-30 01:13:10 +0400

    ///

--- src/androidlearning/AtomGame/GameActivity.java
+++ src/androidlearning/AtomGame/GameActivity.java
@@ -1,14 +1,14 @@
 package androidlearning.AtomGame;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.GridView;
-import android.widget.TextView;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.Map;
 
 /**
  * Created by Olga on 25.07.2014.
@@ -19,44 +19,82 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
 
-        GridView gameField = (GridView) findViewById(R.id.gameField);
         int xRay = 0;
         int yRay = 0;
 
-        HashMap<Integer, Integer> gamePointMap = setPointOnField();
+        ArrayList<Point> gamePointMap = setPointOnField();
 
-        rayReflection(gameField, gamePointMap, xRay, yRay);
+        rayReflection(gamePointMap, xRay, yRay);
     }
 
     private int x;
     private int y;
 
-    public HashMap<Integer, Integer> setPointOnField() {
+    public ArrayList<Point> setPointOnField() {
         Random r = new Random();
-        HashMap<Integer, Integer> mapPoint = new HashMap<Integer, Integer>();
+        ArrayList<Point> mapPoint = new ArrayList<Point>();
         while (!(mapPoint.size() == 10)) {
-            x = (r.nextInt()*99 + 1);
-            y = (r.nextInt()*99 + 1);
-            if (!(mapPoint.containsKey(x) && mapPoint.containsValue(y))) {
-                mapPoint.put(x, y);
+            x = (r.nextInt()*9 + 1);
+            y = (r.nextInt()*9 + 1);
+            if (!(mapPoint.contains(new Point(x, y)))) {
+                mapPoint.add(new Point(x, y));
             }
         }
 
         return mapPoint;
     }
 
-    public void rayReflection(GridView field, HashMap<Integer, Integer> mapPoint, int xRay, int yRay) {
-        for (Map.Entry<Integer, Integer> pair : mapPoint.entrySet()) {
-            int x = pair.getKey();
-            int y = pair.getValue();
-            if ((mapPoint.containsKey(xRay)) || (mapPoint.containsValue(yRay))) {
+    protected void onDraw(Canvas canvas) {
+        int interval = canvas.getWidth()/12;
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
 
-            }
-            else {
-                if ((x == xRay - 1) || (x == xRay + 1)) {
-                    yRay = y - 1;
+        paint.setColor(Color.WHITE);
+        canvas.drawPaint(paint);
+
+        int xStart = 0;
+        int yStart = 0;
+        int xFinish = 0;
+        int yFinish = canvas.getHeight();
+        paint.setColor(Color.BLACK);
+
+        for (int i = 0; i < 10; i++) {
+            canvas.drawLine(xStart, yStart, xFinish, yFinish, paint);
+            xStart += interval;
+            xFinish += interval;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            canvas.drawLine(xStart, yStart, xFinish, yFinish, paint);
+            yStart += interval;
+            yFinish += interval;
+        }
+    }
+
+    public void rayReflection(ArrayList<Point> mapPoint, int startXRay, int startYRay) {
+
+        int finishXRay;
+        int finishYRay;
+
+        while (true) {
+            if (mapPoint.get(Point.x).equals(0)) {
+                startYRay = Point.y;
+                if (mapPoint.contains(Point.startYRay)) {
+                    finishYRay = y - 1;
+                    finishXRay = ;
+                    rayReflection(mapPoint, finishXRay, finishYRay);
+                } else {
+                    finishYRay = startYRay;
+                    break;
                 }
-                else if ((y == yRay - 1) || (y == yRay + 1)) {
+            } else (mapPoint.get(Point.y).equals(0)) {
+                startXRay = Point.x;
+                if (mapPoint.contains(Point.startXRay)) {
+                    finishXRay = x - 1;
+                    rayReflection(field, mapPoint, finishXRay, finishYRay);
+                } else {
+                    finishXRay = startXRay;
+                    break;
                 }
             }
         }
